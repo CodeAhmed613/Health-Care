5dfa845253104e155b742a9939b9c02fbf14ab9f
Ahmed Hassen <ahmdhasn613@gmail.com>
2026-03-09 16:52:36 +0300

    Use environment variable for backend API

--- client/src/api.js
+++ client/src/api.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-// Change 'localhost' to your actual computer IP
-const API_BASE = process.env.REACT_APP_API_URL || 'http://192.168.8.189:5000';
+// Use environment variable if available (for production on Vercel)
+//const API_BASE = process.env.REACT_APP_API_URL || 'http://192.168.8.189:5000';
 
 const API = axios.create({
-    baseURL: API_BASE,
+    baseURL: process.env.REACT_APP_API_URL, // dynamically uses either local or production
     headers: {
         'Content-Type': 'application/json'
     }
